e2265c5768c064da7e2b1e95e8856c2f369f13e3
Adriel Teles <adrielteles@outlook.com>
2023-10-16 19:44:16 -0300

    Starting project

---
+++ cryptoSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cryptoSearch
+//
+//  Created by Adriel Teles on 16/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
