64d636c1c5ca630c4e9b6c0fe2adfe4fa44003c6
Adriel Teles <adrielteles@outlook.com>
2023-10-17 11:59:44 -0300

    ...

--- cryptoSearch/enum/APIEnumExemple.swift
+++ cryptoSearch/enum/APIEnumExemple.swift
@@ -9,6 +9,7 @@ import Foundation
 
 // Rename class end file Name to "API" and replace "YOUR API KEY" to your apikey
 // Get your api key on link: https://www.coinapi.io/
+// teste
 enum APIEnumExemple {
     static let BASE_URL = "https://rest.coinapi.io/v1/exchangerate/BTC"
     static let API_KEY = "YOUR API KEY"
